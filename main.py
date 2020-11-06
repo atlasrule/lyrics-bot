27b472efb375e81019223f52decbeb54fcd9e9ef
Berkay Yılmaz <beko757@gmail.com>
2020-11-06 14:21:29 +0000

    Added main loop

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os, json, re, spotipy
+import os, re, spotipy, lyricsgenius, tweepy
 from random import choice
 from time import sleep
-
-os.system("pip3 install lyricsgenius")
-os.system("pip3 install tweepy")
-
-import lyricsgenius
-import tweepy
 
 ##############################
 #### Spotify API
@@ -21,61 +15,67 @@ scope = 'user-read-currently-playing'
 
 token = spotipy.util.prompt_for_user_token( username, scope)
 
-sp = spotipy.Spotify(auth=token)
-current_song = sp.currently_playing()
+last_tweeted = ""
 
-artist_name = current_song['item']['artists'][0]['name']
+while True: # Iterates every 2 minutes checks for a song
 
-song_name = current_song['item']['name']
+  sp = spotipy.Spotify(auth=token)
+  current_song = sp.currently_playing()
 
-dump = json.dumps(current_song.get("item"), indent=4)
+  if current_song == None:
+    print("\nNot listening anything.\n")
+    sleep(60 * 60 * 2)
+    continue
 
-#print(dump)
+  artist_name = current_song['item']['artists'][0]['name']
 
-print("\n", "   Artist name:", artist_name, end="\n\n")
+  song_name = current_song['item']['name']
 
-print(     "    Song name:", song_name, end="\n\n")
+  if song_name == last_tweeted:
+    print("\nSong already tweeted.\n")
+    sleep(60 * 60 * 2)
+    continue
+    
+  print("\n", "   Artist name:", artist_name, end="\n\n")
+  print(     "    Song name:", song_name, end="\n\n")
 
-##############################
-#### Genius API
+  ##############################
+  #### Genius API
 
-GENIUS_CLIENT_ID = "MM9djKni9Mquv0klm0peaYuGvH3r2wT13KxaF1rSX3N9hNA-6DYWU90PhqkBaLgh"
-genius = lyricsgenius.Genius(GENIUS_CLIENT_ID)
+  GENIUS_CLIENT_ID = "MM9djKni9Mquv0klm0peaYuGvH3r2wT13KxaF1rSX3N9hNA-6DYWU90PhqkBaLgh"
+  genius = lyricsgenius.Genius(GENIUS_CLIENT_ID)
 
-song = genius.search_song(song_name, artist_name)
+  song = genius.search_song(song_name, artist_name)
 
-try:
-    lyrics = song.lyrics
-except ( AttributeError):
-    exit(0)
+  lyrics = song.lyrics
 
-##############################
-#### Create tweet
+  ##############################
+  #### Create tweet's text
 
-lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)  # Remove lyric identifiers
-lyrics = lyrics.replace('[','').replace(']','')
+  lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)  # Remove lyric identifiers
+  lyrics = lyrics.replace('[','').replace(']','')
 
-lyrics = re.sub(r'[\n]{2,}', '\n\n', lyrics)  # Standardize paragraph spacing
+  lyrics = re.sub(r'[\n]{2,}', '\n\n', lyrics)  # Standardize paragraph spacing
 
-lyrics = lyrics.split("\n\n")  # Split lyrics into paragraphs
+  lyrics = lyrics.split("\n\n")  # Split lyrics into paragraphs
 
-for paragraph in lyrics:
-  print(paragraph, "\n\n","*"*25)
+  for paragraph in lyrics:
+    print(paragraph, "\n\n","*"*25)
 
-tweet = choice(lyrics) + "\n\n({} - {})".format(artist_name, song_name)
+  tweet = choice(lyrics) + "\n\n({} - {})".format(artist_name, song_name)
 
-##############################
-#### Twitter API
+  ##############################
+  #### Twitter API
 
-consumer_key =    'nsObgpOuFpytQDX6YQdXgTzLB'
-consumer_secret = '48A68WvbO7YTOBoXZ566CCJE0RBzRuMuucS5K2V4jGW3t0LeOY'
-access_token =    '1324426316695932928-M42ziiKmoThiEkS1C401eKcv2CVz6M'
-access_secret =   'qKN5hl9bQq4ret79uwpz6Cq7jBNzt87h4ulYf0w7KChSk'
+  consumer_key =    'nsObgpOuFpytQDX6YQdXgTzLB'
+  consumer_secret = '48A68WvbO7YTOBoXZ566CCJE0RBzRuMuucS5K2V4jGW3t0LeOY'
+  access_token =    '1324426316695932928-M42ziiKmoThiEkS1C401eKcv2CVz6M'
+  access_secret =   'qKN5hl9bQq4ret79uwpz6Cq7jBNzt87h4ulYf0w7KChSk'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_secret)
+  api = tweepy.API(auth)
 
-api.update_status(tweet)  # Pick random paragraph and tweet
+  api.update_status(tweet)  # Tweet the verse
 
-sleep(60 * 60 * 30)
+  last_tweeted = song_name
