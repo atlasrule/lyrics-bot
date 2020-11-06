1ced718de0cdd764051d6c2d4deb7e71d626d06d
Berkay Yılmaz <beko757@gmail.com>
2020-11-06 16:37:57 +0000

    Fixed tweet string problems

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import os, re, spotipy, lyricsgenius, tweepy
 from random import choice
 from time import sleep
 
-tweet_frequency_mins = int(os.getenv("TWEET_FREQUENCY_MINS"))
+TWEET_FREQUENCY_MINS = int(os.getenv("TWEET_FREQUENCY_MINS"))
+
+SPOTIFY_USERNAME = os.getenv("USERNAME")
 
 GENIUS_CLIENT_ID = os.getenv("GENIUS_CLIENT_ID")
 
-tw_consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
-tw_consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
-tw_access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-tw_access_secret = os.getenv("TWITTER_ACCESS_SECRET")
+TWITTER_CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
+TWITTER_CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
 
 ##############################
 #### Spotify API
 
-username = "bekoo757"
-
 scope = 'user-read-currently-playing'
 
-token = spotipy.util.prompt_for_user_token( username, scope)
+token = spotipy.util.prompt_for_user_token( SPOTIFY_USERNAME, scope)
 
 last_tweeted = ""
 
@@ -29,7 +29,7 @@ while True: # Iterates every 2 minutes checks for a song
 
   if current_song == None:
     print("\nNot listening anything.\n")
-    sleep(60 * tweet_frequency_mins)
+    sleep(60 * TWEET_FREQUENCY_MINS)
     continue
 
   artist_name = current_song['item']['artists'][0]['name']
@@ -38,7 +38,7 @@ while True: # Iterates every 2 minutes checks for a song
 
   if song_name == last_tweeted:
     print("\nSong already tweeted.\n")
-    sleep(60 * tweet_frequency_mins)
+    sleep(60 * TWEET_FREQUENCY_MINS)
     continue
     
   print("\n", "   Artist name:", artist_name, end="\n\n")
@@ -55,7 +55,7 @@ while True: # Iterates every 2 minutes checks for a song
     lyrics = song.lyrics
   except ( AttributeError):
     print("\nCouldn't get lyrics.\n")
-    sleep(60 * tweet_frequency_mins)
+    sleep(60 * TWEET_FREQUENCY_MINS)
     continue
 
   ##############################
@@ -73,20 +73,20 @@ while True: # Iterates every 2 minutes checks for a song
 
   tweet = choice(lyrics) + "\n\n({} - {})".format(artist_name, song_name)
 
-  if len(tweet) >= 280:
-    trimmed_tweet = tweet.strip().split("\n")[0:4]  # First 4 lines
-    trimmed_tweet += tweet.strip().split("\n")[-1]  # Last Line
-    tweet = trimmed_tweet
+if len(tweet) >= 280:
+  trimmed_tweet = trimmed_tweet = "\n".join(tweet.strip().split("\n")[0:4])  # First 4 lines
+  trimmed_tweet += "\n\n" + tweet.strip().split("\n")[-1]  # Last Line
+  tweet = trimmed_tweet
 
   ##############################
   #### Twitter API
 
-  auth = tweepy.OAuthHandler(tw_consumer_key, tw_consumer_secret)
-  auth.set_access_token(tw_access_token, tw_access_secret)
+  auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+  auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
   api = tweepy.API(auth)
 
   api.update_status(tweet)  # Tweet the verse
 
   last_tweeted = song_name
 
-  sleep(60 * tweet_frequency_mins)
+  sleep(60 * TWEET_FREQUENCY_MINS)
