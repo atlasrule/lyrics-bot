1f45626a2887ef0fb8fea723ee9f31295ebe963c
Berkay Yılmaz <beko757@gmail.com>
2020-11-08 15:39:48 +0000

    Added follow-back feature

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import os, re, spotipy, lyricsgenius, tweepy
 from random import choice
 from time import sleep
 
+def follow_back():
+  followers=[api.followers()]
+  following=[api.friends()]
+  for follower in followers:
+      if not follower in following:            
+          api.create_friendship(follower)
+          print('Followed ' + follower.screen_name + ' back.')
+
 TWEET_FREQUENCY_MINS = int(os.getenv("TWEET_FREQUENCY_MINS"))
 
 SPOTIFY_USERNAME = os.getenv("USERNAME")
@@ -84,5 +92,8 @@ while True: # Iterates every 2 minutes checks for a song
   api.update_status(tweet)  # Tweet the verse
 
   last_tweeted = song_name
+
+
+  follow_back()
 
   sleep(60 * TWEET_FREQUENCY_MINS)
